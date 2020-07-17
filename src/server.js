9c88b175e88f72ac3268bc3914c03b4347f186a5
Nitzeli <ntzeli_alejandra@hotmail.com>
2020-07-16 20:26:57 -0500

    router mentores

--- src/server.js
+++ src/server.js
@@ -7,15 +7,18 @@ const app = express ()
 
 const kodersRouter = require ('./routes/koders')
 
+const mentorsRouter = require('./routes/mentors') 
+
 app.use(express.json())
 
 // montando el router de koders
 
 app.use('/koders', kodersRouter)
+app.use('/mentors',mentorsRouter)
 
-app.get ('/', (request, response) =>{
-    response.json ({
-        success: true,
+app.get('/', (request,response)=>{
+    response.json({
+        succes: true,
         message: 'Kodemia APIv8'
     })
 })
